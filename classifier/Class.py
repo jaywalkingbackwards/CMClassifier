da11152c383e040f78017310769f442b81712dff
Alexander <yourdestroer@gmail.com>
2018-11-04 21:30:23 +0300

    Add files via upload

---
+++ classifier/Class.py
@@ -0,0 +1,16 @@
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pickle
+
+class Classifier:
+    def __init__(self):
+        with open('text_classifier', 'rb') as training_model:
+            self.trained_model = pickle.load(training_model)
+        with open('vectorizer', 'rb') as vct:
+            self.vectorizer1 = pickle.load(vct)
+    def text(self,X_test):
+        y_pred = self.trained_model.predict(self.vectorizer1.transform([X_test]))
+        return y_pred
